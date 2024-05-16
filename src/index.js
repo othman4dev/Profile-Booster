3d1ecc08ee2fdfa0987ebac344cf263507357b4a
othman4dev <othmandevsup@gmail.com>
2024-05-16 12:04:00 +0100

    Optimize image loading times

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-switch (error) {
-    case 5:
-        console.log("el case 1");
-        break;
-    default:
-        console.log("Default case");
+for (let i = 0; i < 10; i++) {
+    console.log("Loop iteration: " + i);
+    e.preventDefault();
+    data.push(value);
+    console.log('Processing data...');
 }
