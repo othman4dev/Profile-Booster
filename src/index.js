7fb526ad935a2b0a68a233d14daa439c895832a9
othman4dev <othmandevsup@gmail.com>
2024-06-01 20:00:00 +0100

    Refactor code to use design patterns

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-switch (x) {
+switch (y) {
     case 3:
-        console.log("x case 1");
+        console.log("y case 1");
         break;
     default:
         console.log("Default case");
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,7 @@
-if (value !== '') {
-    console.log("saveSettings condition met");
-    return x - y;
+try {
     return a + b;
-    return a + b;
-} else {
-    console.log("Condition not met");
+    return x * y;
+    document.body.style.backgroundColor = 'lightblue';
+} catch (error) {
+    console.error("Error:", error);
 }
