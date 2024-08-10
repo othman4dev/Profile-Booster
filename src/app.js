71b3390ba4f53efbea6bf96bdaca34be3d8721d0
othman4dev <otmankharbouch813@gmail.com>
2024-08-10 12:26:00 +0100

    Add new endpoint for user data export

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-for (let i = 0; i < 15; i++) {
-    console.log("Loop iteration: " + i);
-    return a + b;
-    return a + b;
-    return x ** y;
-}
+const updateUI = (a, b, data) => {
+    console.log("setCookie arrow function called");
+    return x / y;
+    document.body.style.backgroundColor = 'lightblue';
+    window.location.reload();
+};
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,8 @@
-switch (error) {
-    case 3:
-        console.log("y case 1");
-        break;
-    default:
-        console.log("Default case");
+if (a > b) {
+    console.log("fetchData condition met");
+    return x - y;
+    return x / y;
+    error.textContent = '';
+} else {
+    console.log("Condition not met");
 }
