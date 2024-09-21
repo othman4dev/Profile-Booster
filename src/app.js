a636d437729b783dc8e2667e58086391f12eba41
othman4dev <otmankharbouch813@gmail.com>
2024-09-21 18:57:00 +0100

    Fix accessibility issues for screen readers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-switch (data) {
-    case 1:
-        console.log("el case 1");
-        break;
-    default:
-        console.log("Default case");
+function createChart(a, b) {
+    console.log("saveSettings called");
+    window.location.reload();
+    return x % y;
+    return x ** y;
 }
